88dd39458641e692647534d6796dcf2c53cf7444
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-22 20:26:57 +0200

    Turn the light on when plugins is loaded and off when it's unloaded

--- src/main/java/in/aerem/AlicePlugin/AlicePlugin.java
+++ src/main/java/in/aerem/AlicePlugin/AlicePlugin.java
@@ -2,15 +2,81 @@ package in.aerem.AlicePlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class AlicePlugin extends JavaPlugin {
-    public AlicePlugin(){}
+    public AlicePlugin() {
+    }
 
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
+        switchLight(true);
     }
+
     @Override
     public void onDisable() {
         getLogger().info("onDisable is called!");
+        switchLight(false);
+    }
+
+    public static void switchLight(Boolean on) {
+        if (on) {
+            executePost("{ \"on\": true }");
+        } else {
+            executePost("{ \"on\": false }");
+        }
+    }
+
+    public static String executePost(String urlParameters) {
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            //Create connection
+            url = new URL("http://hue-bridge/api/oWAlM9UyEqI41ABIYvfTXltr5Fu80xTUpwP2lBbg/lights/5/state");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setRequestProperty("Content-Length", "" +
+                    Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
