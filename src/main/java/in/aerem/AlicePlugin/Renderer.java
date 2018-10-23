55fce74afd5aa3df824eac41319f17680572ce47
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-24 00:49:07 +0200

    Remove unused import

--- src/main/java/in/aerem/AlicePlugin/Renderer.java
+++ src/main/java/in/aerem/AlicePlugin/Renderer.java
@@ -8,7 +8,6 @@ import org.bukkit.map.MapView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
