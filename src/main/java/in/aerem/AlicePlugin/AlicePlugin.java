94cd7630df382d652b7c689e71ae69df242df171
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-24 01:14:22 +0200

    Set up dummy webserver

--- src/main/java/in/aerem/AlicePlugin/AlicePlugin.java
+++ src/main/java/in/aerem/AlicePlugin/AlicePlugin.java
@@ -1,6 +1,7 @@
 package in.aerem.AlicePlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import spark.Spark;
 
 public class AlicePlugin extends JavaPlugin {
     public AlicePlugin() {
@@ -11,11 +12,14 @@ public class AlicePlugin extends JavaPlugin {
         getLogger().info("onEnable is called!");
         LightsController.switchLight(true);
         getServer().getPluginManager().registerEvents(new InteractListener(getLogger()), this);
+        Spark.init();
+        Spark.post("/command", (request, response) -> "");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("onDisable is called!");
+        Spark.stop();
         LightsController.switchLight(false);
     }
 }
