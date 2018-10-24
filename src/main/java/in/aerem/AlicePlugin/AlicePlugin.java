e9a932c2b0a67a3c7b12b29bf85647892e95cc40
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-24 21:33:29 +0200

    Use JSON as /command input

--- src/main/java/in/aerem/AlicePlugin/AlicePlugin.java
+++ src/main/java/in/aerem/AlicePlugin/AlicePlugin.java
@@ -1,11 +1,14 @@
 package in.aerem.AlicePlugin;
 
+import com.google.gson.Gson;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandException;
 import org.bukkit.plugin.java.JavaPlugin;
 import spark.Spark;
 
 public class AlicePlugin extends JavaPlugin {
+    private static Gson gson = new Gson();
+
     public AlicePlugin() {
     }
 
@@ -16,7 +19,7 @@ public class AlicePlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new InteractListener(getLogger()), this);
         Spark.init();
         Spark.post("/command", (request, response) -> {
-            String command = request.body();
+            String command = gson.fromJson(request.body(), CommandRequest.class).command;
             getLogger().info("Executing command: " + command);
             getServer().getScheduler().runTask(this, () -> {
                 try {
---
+++ src/main/java/in/aerem/AlicePlugin/CommandRequest.java
@@ -0,0 +1,5 @@
+package in.aerem.AlicePlugin;
+
+public class CommandRequest {
+    public String command;
+}
