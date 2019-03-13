82b3fbb4d1e4fb6b552f56922f7818e77119c4d3
Alexey Eremin <a.eremin.msu@gmail.com>
2019-03-13 23:55:07 +0100

    Add lever handling

--- src/main/java/in/aerem/AlicePlugin/InteractListener.java
+++ src/main/java/in/aerem/AlicePlugin/InteractListener.java
@@ -3,7 +3,9 @@ package in.aerem.AlicePlugin;
 import java.util.logging.Logger;
 
 import com.google.zxing.WriterException;
+import org.bukkit.block.Block;
 import org.bukkit.Color;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -12,7 +14,9 @@ import org.bukkit.event.server.MapInitializeEvent;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
 import org.bukkit.material.MaterialData;
+import org.bukkit.Material;
 import org.bukkit.material.Wool;
+import org.bukkit.material.Lever;
 
 public class InteractListener implements Listener {
     private final Logger logger;
@@ -22,8 +26,7 @@ public class InteractListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event)
-    {
+    public void onPlayerInteract(PlayerInteractEvent event) {
         logger.info("onPlayerInteract");
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK && !event.isBlockInHand()) {
             logger.info(event.getClickedBlock().toString());
@@ -34,6 +37,18 @@ public class InteractListener implements Listener {
                 LightsController.setColor(c.getRed(), c.getGreen(), c.getBlue());
             } else {
                 logger.warning("Not a wool!");
+            }
+        }
+
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+
+        if (block.getType().equals(Material.LEVER)) {
+            Lever lever = (Lever) block.getState().getData();
+            if (lever.isPowered()) {
+                LightsController.setColor(255, 0, 0);
+            } else {
+                LightsController.setColor(0, 255, 0);
             }
         }
     }
